a7a2a3fd60db546b2435bcd1e58dde3a23aeeed7
Luis Pabón <luis.pabon@coreos.com>
2017-07-08 12:33:35 -0400

    added ensure

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ macro_rules! myprintln {
 }
 
     use std::env;
-#[macro_export]
 macro_rules! requiree {
     ($($arg:tt)*) => (if cfg!(debug_assertions) {
         println!("panic: require:\n\tFile {}:{}",
@@ -16,7 +15,43 @@ macro_rules! requiree {
     })
 }
 
+macro_rules! dbc_panic {
+    ($type:expr, $a:expr) => (if cfg!(debug_assertions) {
+        println!("panic: {}: \nfile: {}:{}",
+            $type, file!(), line!());
+        env::set_var("RUST_BACKTRACE", "1");
+        assert!($a);
+    });
+    ($type:expr, $a:expr, $($args:tt)*) => (if cfg!(debug_assertions) {
+        println!("panic: {}: \nfile: {}:{}",
+            $type, file!(), line!());
+        println!("vars:\n{}", sv!($($args)*));
+        env::set_var("RUST_BACKTRACE", "1");
+        assert!($a);
+    })
+}
+
+#[macro_export]
 macro_rules! require {
+    ($a:expr) => (if cfg!(debug_assertions) {
+        dbc_panic!("REQUIRE", $a)
+    });
+    ($a:expr, $($args:tt)*) => (if cfg!(debug_assertions) {
+        dbc_panic!("REQUIRE", $a, $($args)*)
+    })
+}
+
+#[macro_export]
+macro_rules! ensure {
+    ($a:expr) => (if cfg!(debug_assertions) {
+        dbc_panic!("ENSURE", $a)
+    });
+    ($a:expr, $($args:tt)*) => (if cfg!(debug_assertions) {
+        dbc_panic!("ENSURE", $a, $($args)*)
+    })
+}
+
+macro_rules! requirev {
     ($a:expr, $($args:tt)*) => (if cfg!(debug_assertions) {
         println!("panic: REQUIRE: File {}:{}",
             file!(), line!());
@@ -33,9 +68,8 @@ struct AA(i32);
 struct BB(AA);
 
 macro_rules! sv {
-    ($var:ident) => (format!("{}={:?} ", stringify!($var), $var));
-
-    ($var:ident, $($arg:tt)*) => (format!("{} {}", sv!($var), sv!($($arg)*)));
+    ($var:ident) => (format!("   {}={:?}\n", stringify!($var), $var));
+    ($var:ident, $($arg:tt)*) => (format!("{}{}", sv!($var), sv!($($arg)*)));
 }
 
 macro_rules! sv_save {
@@ -50,12 +84,15 @@ macro_rules! pv {
 fn main() {
     let a = 34;
     let s = BB(AA(234));
-    println!("---> a = {}", a);
+    let c = "ehloo";
+    let f = 'f';
+    //println!("---> a = {}", a);
     //pv!(a);
     //pv!(s);
-    println!("{}", sv!(a, s));
+    //println!("{}", sv!(a, s));
     //println!(vv!(a));
     //require!(false, "here is ia", "lot of information");
-    require!(a == 32, a, s);
+    ensure!(a==43,a);
+    require!(a == 32, a, s, c, f);
     myprintln!("Hello, world!");
 }
